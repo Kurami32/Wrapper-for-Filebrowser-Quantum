aafa40a547ec7aace6cfae1f9f22b8933b9cc94f
Kevin B. <kurami32_kbm@proton.me>
2026-08-11 12:43:16 -0500

    use built-in kotlin and try to address some coderabbit comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import com.android.build.api.dsl.ApplicationExtension
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 configure<ApplicationExtension> {
